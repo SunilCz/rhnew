8a74bba4956beb65d09ca1ed1edde2d5a27a32db
SunilCz <sunil.chaudhary7281@gmai.com>
2023-12-04 14:52:10 +0545

    first commit

---
+++ src/components/Emit/Emit.js
@@ -0,0 +1,26 @@
+// import React from 'react';
+
+// const Emit = ({ locationData, sharedLocation }) => {
+//   return (
+//     <div className='emit-container'>
+//       <h2>Location Data</h2>
+//       {locationData && (
+//         <div>
+//           <p>Latitude: {locationData.latitude}</p>
+//           <p>Longitude: {locationData.longitude}</p>
+//         </div>
+//       )}
+
+//       <h2>Shared Location</h2>
+//       {sharedLocation && (
+//         <div>
+//           <p>Participant ID: {sharedLocation.participantId}</p>
+//           <p>Latitude: {sharedLocation.latitude}</p>
+//           <p>Longitude: {sharedLocation.longitude}</p>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Emit;
